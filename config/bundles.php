25581979b288a5911baae64d97346d27a908daff
Stanley Sathler <stanleysathlerpinto@gmail.com>
2018-12-20 23:26:00 -0200

    Add DoctrineFixtures as a dependency (even for prod environment)
    Since there is not any way to create a new account (no routes for that),
    we need to run the "sge:database:reinitialize" composer command whenever
    we deploy a new prod app.
    
    That said, we decided to move the DoctrineFixtures dependency from dev
    dependencies to prod dependencies, at least for now.

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,5 @@ return [
     PUGX\AutocompleterBundle\PUGXAutocompleterBundle::class => ['all' => true],
     Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'prod' => true],
 ];
