c863d76077a9fa5d9956eff6554d0a72dc3a0367
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-01-15 22:49:40 -0200

    Remove Autocompleter and Select2Entity bundles

--- config/bundles.php
+++ config/bundles.php
@@ -14,8 +14,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    PUGX\AutocompleterBundle\PUGXAutocompleterBundle::class => ['all' => true],
-    Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'prod' => true],
 ];
